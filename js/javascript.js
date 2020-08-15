c6f1d9f537ae2e57a04cdfbe83ad7085cca263f6
Klaybson M. Conceição <49767964+Klaybson@users.noreply.github.com>
2020-08-14 21:51:04 -0300

    update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,9 @@
+//Mensagem  pog up
+	window.alert('Primeira msg')
+	//Mensagem com confirmação
+	window.confirm('Tá gostando?')
+	//Mensagem com prompt
+	window.prompt('Qual seu nome?')
 
+// Passando valor para uma variavel 
+// var m = Number.percentInt(Window.prompt('Digite um numero'))
